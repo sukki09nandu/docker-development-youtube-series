86dbc5cda0e99844acbb334e236e2eae9dcdd311
sukki09nandu <sukki09nandu@gmail.com>
2025-03-26 20:24:54 -0400

    python app

---
+++ Sukheshrepo/Introduction/src/app.py
@@ -0,0 +1,9 @@
+
+def getCustomer(customerID):
+    firstName= "Marcel"
+    lastName= "Dempers"
+    FullName = firstName +" "+ lastName
+    return "Customer ID:" + customerID +" Name:"+ FullName
+
+#print(firstName)
+print(getCustomer("743"))
